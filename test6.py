86702caca64db305df308b6c27f06afa8f154b60
Nurullah Ferman CAMCI <nurullahcmc@yahoocom>
2024-06-06 23:58:24 +0300

    sadsa

---
+++ test6.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+# Calling the function with two numbers
+sum = add_numbers(3, 5)
+
+# Printing the result
+print(f"The sum of 3 and 5 is {sum}")
